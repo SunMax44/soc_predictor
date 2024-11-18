1dc01baf2c21206204c5aed0d6c4e383df186e51
SunMax44 <maxsonntag@posteo.de>
2024-11-18 22:00:02 +0100

    file_restructuring

--- data/lucas_db/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the CSV file
-file_path = '/Users/maxsonntag/Documents/GitHub/SOC_predictor/data/lucas_db/location_elevation_retrieved.csv'
-data = pd.read_csv(file_path)
-
-# Print the column names
-print("Column names:", data.columns)
